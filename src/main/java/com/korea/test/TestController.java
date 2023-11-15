4145711877b58957836e2754a9685089917187b8
pogeum <ekglchlrh@gmail.com>
2023-11-15 17:42:12 +0900

    웹개발 심화 평가

--- src/main/java/com/korea/test/TestController.java
+++ src/main/java/com/korea/test/TestController.java
@@ -24,6 +24,11 @@ public class TestController {
         //1. DB에서 데이터 꺼내오기
         List<Post> postList = postRepository.findAll();
 
+        if (postList.isEmpty()) {
+            saveDefault();
+            return "redirect:/";
+        }
+
         //2. 꺼내온 데이터를 템플릿으로 보내기
         model.addAttribute("postList", postList);
         model.addAttribute("targetPost", postList.get(0));
@@ -33,12 +38,8 @@ public class TestController {
 
     @PostMapping("/write")
     public String write() {
-        Post post = new Post();
-        post.setTitle("new title..");
-        post.setContent("");
-        post.setCreateDate(LocalDateTime.now());
 
-        postRepository.save(post);
+        saveDefault();
 
         return "redirect:/";
     }
@@ -54,10 +55,29 @@ public class TestController {
     @PostMapping("/update")
     public String update(Long id, String title, String content) {
         Post post = postRepository.findById(id).get();
+
+        if (title.trim().length() == 0) {
+            title = "제목 없음";
+        }
+
         post.setTitle(title);
         post.setContent(content);
 
         postRepository.save(post);
         return "redirect:/detail/" + id;
+    }
+
+    @PostMapping("delete")
+    public String delete(Long id) {
+        postRepository.deleteById(id);
+        return "redirect:/";
+    }
+
+    private void saveDefault() {
+        Post post = new Post();
+        post.setTitle("new title..");
+        post.setContent("");
+        post.setCreateDate(LocalDateTime.now());
+        postRepository.save(post);
     }
 }
